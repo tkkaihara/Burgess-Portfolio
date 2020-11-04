b5467758eded4d6b28f549b26d7b8f9fdeb4372b
Tyler Kaihara <kenzokaihara@gmail.com>
2020-11-03 16:29:10 -0800

    reformat logo images

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -24,7 +24,7 @@ export default function AppDesigns() {
             </div>
             <div className='logos-menu-item'>
               <ScrollLink
-                className=' font-size-1_3'
+                className='font-size-1_3'
                 to='vertical-ranch'
                 smooth={true}
                 duration={800}>
@@ -48,15 +48,19 @@ export default function AppDesigns() {
             aim to convey the passion for this idea to clean up the trails in
             the Pacific Northwest.
           </p>
-          <img
-            src={OutdoorBurlapStart}
-            alt='Outdoor Burlap start'
-            className='process-picture-vertical'></img>
-          <br></br>
-          <img
-            src={OutdoorBurlapLogo}
-            alt='Outdoor Burlap logo'
-            className='process-picture-vertical'></img>
+
+          <div className='logo-pictures mt-5'>
+            <p className='font-size-1_3'>Before:</p>
+            <p className='font-size-1_3'>After:</p>
+            <img
+              src={OutdoorBurlapStart}
+              alt='Outdoor Burlap start'
+              className='circle logo-picture'></img>
+            <img
+              src={OutdoorBurlapLogo}
+              alt='Outdoor Burlap logo'
+              className='logo-picture'></img>
+          </div>
         </div>
       </section>
 
@@ -74,15 +78,18 @@ export default function AppDesigns() {
             landscape that I love, but the feeling of fun and the great
             outdoors.
           </p>
-          <img
-            src={VerticalRanchStart}
-            alt='Vertical Ranch start'
-            className='process-picture-vertical'></img>
-          <br></br>
-          <img
-            src={VerticalRanchLogo}
-            alt='Vertical Ranch logo'
-            className='process-picture-vertical'></img>
+          <div className='logo-pictures mt-5'>
+            <p className='font-size-1_3'>Before:</p>
+            <p className='font-size-1_3'>After:</p>
+            <img
+              src={VerticalRanchStart}
+              alt='Vertical Ranch start'
+              className='circle logo-picture'></img>
+            <img
+              src={VerticalRanchLogo}
+              alt='Vertical Ranch logo'
+              className='logo-picture'></img>
+          </div>
         </div>
       </section>
     </>
